4b0ca47fc78db3bf92649996509308ad1068542e
sombat92 <44734452+sombat92@users.noreply.github.com>
2026-08-19 03:03:34 +0100

    v0.5.10 Added Alembic database migration in production mode + Implemented frontend deployment

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+set -e
 
 if [ "$PRODUCTION" = "true" ]; then
+    echo "Running database migrations..."
+    alembic upgrade head
+    
     echo "Starting in production mode..."
     exec uvicorn app.main:app \
         --host 0.0.0.0 \
